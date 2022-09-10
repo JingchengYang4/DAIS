9ec2aa5c64766936bde484cff0368e29a13c3166
JingchengYang4 <JingchengYang4@icloud.com>
2022-09-10 21:29:18 +0800

    improved mode1

--- detectron2/modeling/roi_heads/edge_occlusion_head.py
+++ detectron2/modeling/roi_heads/edge_occlusion_head.py
@@ -16,24 +16,26 @@ class Edge_Occlusion(nn.Module):
         self.fconv3 = nn.Conv2d(in_channels=65, out_channels=32, kernel_size=1)
         self.fconv4 = nn.Conv2d(in_channels=33, out_channels=16, kernel_size=1)"""
 
-        self.mode = 3
+        self.mode = 1
 
         if self.mode is 1:
-            self.edge_conv0 = nn.Conv2d(in_channels=1, out_channels=4, kernel_size=1)
-            self.edge_conv1 = nn.Conv2d(in_channels=4, out_channels=8, kernel_size=2, stride=2)#size of 7
-            self.edge_conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=2)#I BELIEVE OUPUT SIZE IS 3
-            self.edge_conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3)
+            self.edge_conv0 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=1)
+            self.edge_conv1 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=2, stride=2)#size of 7
+            self.edge_conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=2)#I BELIEVE OUPUT SIZE IS 3
+            self.edge_conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
 
             # (1-1) x stride + kernel = 3
-            self.dconv0 = nn.ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=3)
+            self.dconv0 = nn.ConvTranspose2d(in_channels=64, out_channels=1, kernel_size=3)
+            self.upconv0 = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=1)
 
-            # (3-1) x stride + kernel = 6
+            # (3-1) x stride + kernel = 7
             # 2 x 2 + 2 = 6
-            self.dconv1 = nn.ConvTranspose2d(in_channels=16, out_channels=8, kernel_size=2, stride=2)
-
-            # (6-1) x stride + kernel = 14
-            # 5 x 2 + 4 = 14
-            self.dconv2 = nn.ConvTranspose2d(in_channels=8, out_channels=4, kernel_size=4, stride=2)
+            self.dconv1 = nn.ConvTranspose2d(in_channels=16, out_channels=8, kernel_size=3, stride=2)
+            self.upconv1 = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=1)
+            # (7-1) x stride + kernel = 14
+            # 6 x 2 + 2 = 14
+            self.dconv2 = nn.ConvTranspose2d(in_channels=8, out_channels=4, kernel_size=2, stride=2)
+            self.upconv2 = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=1)
 
             self.fconv = nn.Conv2d(in_channels=4, out_channels=1, kernel_size=1)
         elif self.mode is 2:
@@ -61,19 +63,32 @@ class Edge_Occlusion(nn.Module):
         if self.mode is 1:
             x = self.edge_conv0(x)
             x = F.relu(x)
+            x0 = x
             x = self.edge_conv1(x)
             x = F.relu(x)
+            x1 = x
             x = self.edge_conv2(x)
             x = F.relu(x)
+            x2 = x
             x = self.edge_conv3(x)
             x = F.relu(x)
 
             x = self.dconv0(x)
             x = F.relu(x)
 
+            x = self.upconv0(torch.cat((x, x0), 1))
+            x = F.relu(x)
+
             x = self.dconv1(x)
             x = F.relu(x)
+
+            x = self.upconv1(torch.cat((x, x1), 1))
+            x = F.relu(x)
+
             x = self.dconv2(x)
+            x = F.relu(x)
+
+            x = self.upconv2(torch.cat((x, x2), 1))
             x = F.relu(x)
 
             x = self.fconv(x)
