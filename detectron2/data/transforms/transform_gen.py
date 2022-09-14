1e3689d36b6ed751290dff7865061d6bebfc7032
Jingcheng <JingchengYang4@icloud.com>
2022-09-14 15:27:42 +0800

    Test

--- detectron2/data/transforms/transform_gen.py
+++ detectron2/data/transforms/transform_gen.py
@@ -436,7 +436,7 @@ def apply_transform_gens(transform_gens, img):
 
     tfms = []
 
-    do_transform = True
+    do_transform = False
     if do_transform:
         for g in transform_gens:
             tfm = g.get_transform(img)
